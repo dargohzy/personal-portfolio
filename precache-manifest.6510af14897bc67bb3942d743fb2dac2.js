16a978519d7db5fbca77641b4ab3c60f66c4ee32
Daryl Goh <dargohzy@users.noreply.github.com>
2020-10-18 00:02:06 +0800

    Updates

---
+++ precache-manifest.6510af14897bc67bb3942d743fb2dac2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74cc084b121f67889171c1b216fcecd",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "c1bf3278f90a49e3835c",
+    "url": "/personal-portfolio/static/css/main.297e0d65.chunk.css"
+  },
+  {
+    "revision": "9d546cff45200817eac4",
+    "url": "/personal-portfolio/static/js/2.fdde3556.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/personal-portfolio/static/js/2.fdde3556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1bf3278f90a49e3835c",
+    "url": "/personal-portfolio/static/js/main.f2516a4d.chunk.js"
+  },
+  {
+    "revision": "1fd8320ed472344e7a3b",
+    "url": "/personal-portfolio/static/js/runtime-main.41dc66a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1fefd4afda09855b3e818f069e067ff8",
+    "url": "/personal-portfolio/static/media/profile-pic.1fefd4af.png"
+  }
+]);
